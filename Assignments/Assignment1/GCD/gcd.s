ace20b899a05a78601e7478ae907792a846dc638
Kusumithavv <38127428+Kusumithavv@users.noreply.github.com>
2018-10-22 18:43:33 +0530

    Update gcd.s

--- Assignments/Assignment1/GCD/gcd.s
+++ Assignments/Assignment1/GCD/gcd.s
@@ -1,1 +1,17 @@
 
+        AREA     appcode, CODE, READONLY
+     export __main	 
+	 ENTRY 
+__main  function
+	          MOV r0 , #12	  ;value of a	
+			  MOV r1 , #60     ;value of b
+LOOP			  CMP r0 , r1
+              IT EQ 
+              BEQ STOP	
+              ITE HI			  
+			  SUBHI r0 , r0 , r1 			  
+			  SUBLS r1 , r1 , r0
+              B LOOP			  
+STOP		      B STOP  ; stop program
+        endfunc
+      end
