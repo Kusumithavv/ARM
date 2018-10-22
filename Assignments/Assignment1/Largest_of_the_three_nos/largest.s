cdd07e7ce12ce8c6429275e2f354deb523cc52f9
Kusumithavv <38127428+Kusumithavv@users.noreply.github.com>
2018-10-22 19:58:49 +0530

    Update largest.s

--- Assignments/Assignment1/Largest_of_the_three_nos/largest.s
+++ Assignments/Assignment1/Largest_of_the_three_nos/largest.s
@@ -6,10 +6,10 @@ __main  function
 	          MOV r0 , #2   ;1st no
 	          MOV r1 , #22    ;2nd no
               MOV r2 , #12 	  ;3rd num 			  
-              CMP r0 , r1
+              CMP r0 , r1         ;compare first 2 nos 
               IT HI
-              MOVHI r1 , r0 
-			  CMP r1 , r2
+              MOVHI r1 , r0       ;move largest to r1
+			  CMP r1 , r2  ; compare largest of first two nums & 3rd num
 			  IT HI 
 			  MOVHI r2 , r1
 			  MOV r3 , r2         ;result in r3
