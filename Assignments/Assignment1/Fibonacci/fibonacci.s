b8d197ddca318a407a84e29bb1e0c131ea4d4c5c
Kusumithavv <38127428+Kusumithavv@users.noreply.github.com>
2018-10-22 19:55:03 +0530

    Update fibonacci.s

--- Assignments/Assignment1/Fibonacci/fibonacci.s
+++ Assignments/Assignment1/Fibonacci/fibonacci.s
@@ -1,1 +1,25 @@
 
+     AREA     appcode, CODE, READONLY
+     export __main	 
+	 ENTRY 
+__main  function
+          MOV r0 , #0  ; 1st no in series
+	          MOV r1 , #1    ; 2nd no in series
+              MOV r7 , #9	  ;  calculate fibonacci series upto 9 numbers
+              MOV r2 , r7	 ; result in r2		  
+              CMP r2 , #1  ; repeat until r2=1
+              IT LS 
+              BLS STOP				  
+			  SUB r3 , r2 ,#1      ; loop
+fibloop             
+                  ADD r4 , r1 , r0   
+                  MOV r0 ,r1
+                  MOV r1 ,r4
+                  MOV r2 , r4
+                  SUB r3 ,#1 
+				  CMP r3 ,#0
+                  BNE fibloop
+ 
+STOP		      B STOP  ; stop program
+        endfunc
+      end
