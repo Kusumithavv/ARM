787e9cb510b6b8325c44788c73b560e3e96af2a4
Kusumithavv <38127428+Kusumithavv@users.noreply.github.com>
2018-10-22 19:13:36 +0530

    Update largest.s

--- Assignments/Assignment1/Largest_of_the_three_nos/largest.s
+++ Assignments/Assignment1/Largest_of_the_three_nos/largest.s
@@ -1,1 +1,19 @@
 
+     AREA     appcode, CODE, READONLY
+     export __main	 
+	 ENTRY 
+__main  function
+	          MOV r0 , #2   ;1st no
+	          MOV r1 , #22    ;2nd no
+              MOV r2 , #12 	  ;3rd num 			  
+              CMP r0 , r1
+              IT HI
+              MOVHI r1 , r0 
+			  CMP r1 , r2
+			  IT HI 
+			  MOVHI r2 , r1
+			  MOV r3 , r2         ;result in r3
+STOP		      B STOP  ; stop program
+        endfunc
+      end
+
