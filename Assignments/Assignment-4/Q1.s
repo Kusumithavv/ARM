a713699d24ccad9654845b71702ae6b8a65a753c
Kusumithavv <38127428+Kusumithavv@users.noreply.github.com>
2018-10-30 23:38:26 +0530

    Update Q1.s

--- Assignments/Assignment-4/Q1.s
+++ Assignments/Assignment-4/Q1.s
@@ -5,20 +5,20 @@ PRESERVE8
 	 ENTRY 
 __main  FUNCTION
 
-	VMOV.F32 S1, #4;x-Number to find e^x
-	VMOV.F32 S2, #15;Number of terms been considered in e^x expansion
-	VMOV.F32 S3, #1;count
-	VMOV.F32 S4, #1; temp
-	VMOV.F32 S5, #1; result
-	VMOV.F32 S7, #1;register to hold one
+	VMOV.F32 S1, #4   ; e^4
+	VMOV.F32 S2, #15       ;Number of terms in e^4 expansion
+	VMOV.F32 S3, #1    ;count
+	VMOV.F32 S4, #1    ; temporary variable,t
+	VMOV.F32 S5, #1    ; result stored in s5
+	VMOV.F32 S7, #1    ;register to hold one
 Loop 
-	 VCMP.F32 S2, S3;Comparison done for excuting taylor series expansion of e^x for s2 number of terms
-	 VMRS.F32 APSR_nzcv,FPSCR;Used to copy fpscr to apsr
+	 VCMP.F32 S2, S3   ;Compare for e^x for s2 no of terms
+	 VMRS.F32 APSR_nzcv,FPSCR  ;copy FPSCR to APSR
 	 BLT stop;
-	 VDIV.F32 S6, S1, S3; temp1=x/count
-	 VMUL.F32 S4, S4, S6; temp=temp*temp1;
-	 VADD.F32 S5, S5, S4; result=result+temp;
-	 VADD.F32 S3, S3, S7; incrementing count
+	 VDIV.F32 S6, S1, S3; t1=x/count
+	 VMUL.F32 S4, S4, S6; t=t*t1;
+	 VADD.F32 S5, S5, S4; result=result+t;
+	 VADD.F32 S3, S3, S7; inc count
 	 B Loop; 
 	 
 stop B stop ; stop program
