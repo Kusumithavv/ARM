9161bc8e0924b062b31dfcc8d09430b6b9563597
Kusumithavv <38127428+Kusumithavv@users.noreply.github.com>
2018-11-26 21:10:53 +0530

    Update decrypt.s

--- Hackathon/decrypt.s
+++ Hackathon/decrypt.s
@@ -6,15 +6,15 @@
 __main  FUNCTION
 
 	
-	LDR r6, =0x20012C02 ;location of IV
-	LDR r8 , =0x20012C01 ;location of cipher text
+	LDR r6, =0x20012C02    ;location of IV
+	LDR r8 , =0x20012C01   ;location of cipher text
 	LDR r10,[r6]    ;loading initialization vector to r10
-	LDR r11, [r8] ;loading result from ciphertext location to r11
+	LDR r11, [r8] ;loading result from ciphertext location (of encryped text) to r11
 	CMP r10, #10 ; IV location value = 0 indicates start of msg
 	BEQ STARTMSGD
-	EOR r9,r7,r11
+	EOR r9,r7,r11  
 STARTMSGD 
-	MOV r7 , r11
+	MOV r7 , r11   ; move encryped cipher text to r7
 	EOR r9,r11,#10 ; r9 = exor of ciphertext & initialization vector
       
 stop B stop ; stop program
