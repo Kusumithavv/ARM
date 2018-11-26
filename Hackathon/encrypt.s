32fef9f475b0f0f0167237467ca67b61aecdc8c2
Kusumithavv <38127428+Kusumithavv@users.noreply.github.com>
2018-11-27 00:22:55 +0530

    Update encrypt.s

--- Hackathon/encrypt.s
+++ Hackathon/encrypt.s
@@ -4,9 +4,7 @@
      EXPORT __main
 	 ENTRY 
 __main  FUNCTION
-
-	
-	MOV r4, r0
+ MOV r4, r0
 	LDR r6,=0x20000C02 ;location of IV
 	LDR r8,=0x20005C01 ;location of cipher text
 	MOV r9, #10	;value for IV
@@ -18,12 +16,15 @@ __main  FUNCTION
 	EOR r7,r7,r10 ; exor of plaintext & initialization vector
 	STR r10 , [r6]       ;store IV 
 	STR r7,[r8] ;storing the result
+	ADD r8,r8,#4
 	ADD r4, r4,#4
 ELOOP
 	LDR r7, [r4]  ; reading plain text from start add
 	LDR r11, [r8]  ; load encrypted result to r11
 	EOR r7,r7,r11  ;exor result with plain text
+	ADD r8,r8,#4
 	ADD r4, r4,#4
+	STR r7,[r8]
 	CMP r4,r1
 	BNE ELOOP
       
